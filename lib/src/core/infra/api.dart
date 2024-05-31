b5d9f66454b089319a8484bd5fe59ddc5f75e767
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-05-31 10:23:07 -0300

    feat: api config

---
+++ lib/src/core/infra/api.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  static final String baseUrl = dotenv.env['RICK_AND_MORTY_API_URL'] ?? "";
+
+  get(String path) async {
+    final response = await http.get(Uri.parse("$baseUrl/$path"));
+    return jsonDecode(utf8.decode(response.bodyBytes));
+  }
+}
