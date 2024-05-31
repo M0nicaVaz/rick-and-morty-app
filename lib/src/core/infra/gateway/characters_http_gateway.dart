658bfee8ebbd8569ea827b2bbec7e9de110693c8
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-05-31 10:51:17 -0300

    fix: charactersGateway

--- lib/src/core/infra/gateway/characters_http_gateway.dart
+++ lib/src/core/infra/gateway/characters_http_gateway.dart
@@ -5,7 +5,24 @@ class CharactersGateway {
   final API http;
   CharactersGateway(this.http);
 
-  Future<List<Character>> getCharacters() async {
-    return await http.get("/characters");
+  Future<List<Character>?> getCharacters() async {
+    final response = await http.get('/character');
+    if (response.statusCode == 200) {
+      final data = response.data['results'] as List;
+      return data
+          .map((e) => Character(
+                gender: e["gender"],
+                id: e["id"],
+                image: e["image"],
+                location: e["location"],
+                name: e["name"],
+                origin: e["origin"],
+                species: e["species"],
+                status: e["status"],
+              ))
+          .toList();
+    }
+
+    return null;
   }
 }
