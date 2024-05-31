0a73aacc86b41557153fa0981c550d90d80d5446
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-05-31 14:41:54 -0300

    feat: theme

---
+++ lib/src/config/theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Theme {
+  Color brand000 = const Color(0xffeffff7);
+  Color brand100 = const Color(0xffD3FCE9);
+  Color brand200 = const Color(0xff27EA8F);
+  Color brand300 = const Color(0xff35BD78);
+  Color brand400 = const Color(0xff009A5A);
+  Color brand500 = const Color(0xff009A5A);
+  Color brand600 = const Color(0xff009A5A);
+
+  Color white000 = const Color(0xffffffff);
+
+  Color grey000 = const Color(0xffF7FAFC);
+  Color grey100 = const Color(0xffEDF0F2);
+  Color grey200 = const Color(0xffE1E8ED);
+  Color grey300 = const Color(0xffC2CED6);
+  Color grey400 = const Color(0xff9BA5AB);
+  Color grey500 = const Color(0xff363E46);
+  Color grey600 = const Color(0xff363E46);
+
+  Color red000 = const Color(0xffFFE9E9);
+  Color red100 = const Color(0xffFFBFBF);
+  Color red200 = const Color(0xffFF5B5B);
+  Color red300 = const Color(0xffE64444);
+  Color red400 = const Color(0xffB23535);
+
+  Color yellow000 = const Color(0xffFCFBD4);
+  Color yellow100 = const Color(0xffF7F4B9);
+  Color yellow200 = const Color(0xffF7F161);
+  Color yellow300 = const Color(0xffE9C610);
+  Color yellow400 = const Color(0xffC4A00E);
+
+  Color blue000 = const Color(0xffE2F5FF);
+  Color blue100 = const Color(0xffBEE8FF);
+  Color blue200 = const Color(0xff6BCAFF);
+  Color blue300 = const Color(0xff49B1EB);
+  Color blue400 = const Color(0xff2185D0);
+
+  Color gynecologyObstetrics = const Color(0xff69BD35);
+  Color medicClinic = const Color(0xff1BC8EE);
+  Color pediatry = const Color(0xff356BBD);
+  Color preventiveMedicine = const Color(0xffD83C3C);
+  Color surgery = const Color(0xffFFA24D);
+
+  setTheme() {}
+}
