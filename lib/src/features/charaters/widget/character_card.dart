229b8160896400ec979f3232db68146d23c0e09e
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2024-05-31 23:10:41 -0300

    feat: adding card gradient styles

--- lib/src/features/charaters/widget/character_card.dart
+++ lib/src/features/charaters/widget/character_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rick_and_morty_flutter/src/core/entities/character.dart';
 
 class CharacterCard extends StatelessWidget {
@@ -14,30 +16,95 @@ class CharacterCard extends StatelessWidget {
     return SizedBox(
       width: 205,
       height: 256,
-      child: Card(
-        elevation: 2,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(4),
-                topRight: Radius.circular(4),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(98, 155, 220, 57),
+              Color.fromARGB(133, 0, 150, 135),
+              Color.fromARGB(123, 0, 187, 212),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(4),
+                  topRight: Radius.circular(4),
+                ),
+                child: Image.network(
+                  character.image,
+                  height: 120,
+                ),
               ),
-              child: Image.network(
-                character.image,
-                height: 120,
+              Padding(
+                padding: const EdgeInsets.only(top: 4.0),
+                child: Text(
+                  character.name,
+                  style: GoogleFonts.creepster(
+                      textStyle: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.limeAccent,
+                  )),
+                  softWrap: true,
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 4.0),
-              child: Text(character.name),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 4.0),
-              child: Text(character.species),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      character.status,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      "|",
+                      style: TextStyle(
+                        color: Colors.cyanAccent,
+                      ),
+                    ),
+                    Text(
+                      character.species,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      "|",
+                      style: TextStyle(
+                        color: Colors.cyanAccent,
+                      ),
+                    ),
+                    Text(
+                      character.gender,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
